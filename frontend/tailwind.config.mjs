92475ca7defaaf90f77b627147d5860ccfbf0ad2
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-03-14 23:44:05 +0530

    Initial commit

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,12 @@
+export default {
+    content: [
+      "./pages/**/*.{js,mjs,ts,tsx}",
+      "./components/**/*.{js,mjs,ts,tsx}",
+      "./app/**/*.{js,mjs,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
