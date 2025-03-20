c4edb01f4388783be4a75c15bbb23e430f60c241
Aashiq-Edavalapati <aashiqedavalapati58@gmail.com>
2025-03-21 01:34:19 +0530

    Add landing page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost', 'images.unsplash.com'],
+  },
 };
 
 export default nextConfig;
