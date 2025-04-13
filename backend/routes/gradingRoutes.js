673c18033734a6c9a33eb55931a266a0c4d2879f
Adithya Menon R <adukottan@gmail.com>
2025-04-13 12:18:03 +0530

    Moved grading logic to internal function in classController to prevent internal axios call

--- backend/routes/gradingRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { gradeSubmission } from "../controllers/gradingController.js";
-
-router.post("/grade-submission", gradeSubmission);
-
-export default router;
-
